97d6c7ac5a030841ff19b9b712b1e7429eb56549
JerryHuangCool <905056897@qq.com>
2024-03-04 18:17:19 +0800

    feat: jsx转换实现

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,98 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import {
+	Type,
+	Key,
+	Ref,
+	Props,
+	ReactElementType,
+	ElementType
+} from 'shared/ReactTypes';
+//jsx返回ReactElement结构的数组
+
+//ReactElement构造函数
+const ReactElement = function (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElementType {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'Jerry'
+	};
+	return element;
+};
+
+//React17之前jsx会通过jsx方法运行,之后是React.createElement但其实是一个方法
+//jsx 方法第一个参数接收组件type，第二个参数是配置属性,后续参数可选都作为children处理
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+
+		//对key和ref单独处理
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		//其余参数判断是否在其原型上，只将在其自身上的prop赋值给props
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength > 0) {
+		//children两种情况  一个child， 多个 [child，child，child]
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+	//返回一个新的ReactElement
+	return ReactElement(type, key, ref, props);
+};
+
+//实际react开发环境中的jsx进行了额外的检测,没有第三个参数
+export const jsxDEV = (type: ElementType, config: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
