a315499823b60515836cf2fc5daeede833f67530
JerryHuangCool <905056897@qq.com>
2024-03-12 11:08:30 +0800

    feat: 实现FC渲染

--- packages/react-dom/node_modules/react-reconciler/src/beginWork.ts
+++ packages/react-dom/node_modules/react-reconciler/src/beginWork.ts
@@ -3,8 +3,14 @@
 import { ReactElementType } from 'shared/ReactTypes';
 import { FiberNode } from './fiber';
 import { UpdateQueue, processUpdateQueue } from './upadateQueue';
-import { HostComponent, HostRoot, HostText } from './workTags';
+import {
+	FunctionComponent,
+	HostComponent,
+	HostRoot,
+	HostText
+} from './workTags';
 import { mountChildFibers, reconcilerChildFibers } from './childFibers';
+import { renderWithHooks } from './fiberHooks';
 
 export const beginWork = (wip: FiberNode) => {
 	//比较，最终返回子FiberNode
@@ -18,6 +24,8 @@ export const beginWork = (wip: FiberNode) => {
 		case HostText:
 			//没有子节点
 			return null;
+		case FunctionComponent:
+			return updateFunctionComponent(wip);
 		default:
 			if (__DEV__) {
 				console.warn('beginWork未实现的类型');
@@ -26,6 +34,11 @@ export const beginWork = (wip: FiberNode) => {
 	}
 	return null;
 };
+function updateFunctionComponent(wip: FiberNode) {
+	const nextChildren = renderWithHooks(wip);
+	reconcileChildren(wip, nextChildren);
+	return wip.child;
+}
 
 function updateHostRoot(wip: FiberNode) {
 	const baseState = wip.memoizedState;
