4132940cf7cb43354cadd64fea45d05d5bea7836
JerryHuangCool <905056897@qq.com>
2024-03-21 17:33:33 +0800

    feat: 实现noop-render搭建并发测试环境

--- packages/react-dom/src/hostConfig.ts
+++ packages/react-dom/src/hostConfig.ts
@@ -34,7 +34,9 @@ export function commitUpdate(fiber: FiberNode) {
 		case HostText:
 			const text = fiber.memoizedProps.content;
 			return commitTextUpdate(fiber.stateNode, text);
-
+		case HostComponent:
+			updateFiberProps(fiber.stateNode, fiber.memoizedProps);
+			return;
 		default:
 			if (__DEV__) {
 				console.warn('未实现的Update类型', fiber);
